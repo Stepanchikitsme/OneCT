799d21383139cc2072d805e73fe1fe3305b19d11
dibof228 <85364286+dibof228@users.noreply.github.com>
2025-01-10 18:08:26 +0400

    Update post_buttons.php

--- themes/std/parts/post_buttons.php
+++ themes/std/parts/post_buttons.php
@@ -2,7 +2,8 @@
     <a style="float: left;" href="<?php echo('?id=' .(int)$_GET['id']. '&p=' .(int)$_GET['p'] - 1); ?>">
         <?php echo(lang_back) ?>
     </a>
-<?php elseif(count($data_wall) >= 10): ?>
+<?php endif; ?>
+<?php if(count($data_wall) >= 10): ?>
     <a style="float: right;" href="<?php echo('?id=' .(int)$_GET['id']. '&p=' .(int)$_GET['p'] + 1) ?>">
         <?php echo(lang_forward) ?>
     </a>
